da82c1271ec4fc6c7602060fb5a2c7ae5db9da9d
Pourya <pourya.nejadensan@gmail.com>
2024-07-04 11:42:09 +0200

    Step 4: Override the method from step 3 in the "Car" class to also output the number of doors.

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,3 +1,15 @@
 public class Car extends Vehicle{
     private int numberOfDoors;
+
+    @Override
+    public String showInfo() {
+        return super.showInfo() + "\nCar Number : " + numberOfDoors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "numberOfDoors=" + numberOfDoors +
+                "} " + super.toString();
+    }
 }
