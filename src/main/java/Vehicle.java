eec0258c31a9137fd9b51dce259f0d2355e42fb1
Pourya <pourya.nejadensan@gmail.com>
2024-07-04 11:36:57 +0200

    Step 3: Implement a method in the "Vehicle" class that outputs information about the vehicle.

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -2,4 +2,13 @@ public class Vehicle {
     private String manufacturer;
     private String model;
     private int yearOfManufacture;
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "manufacturer='" + manufacturer + '\'' +
+                ", model='" + model + '\'' +
+                ", yearOfManufacture=" + yearOfManufacture +
+                '}';
+    }
 }
